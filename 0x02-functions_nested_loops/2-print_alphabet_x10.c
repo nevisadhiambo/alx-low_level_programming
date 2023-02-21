b9be80b9835991493671c11d0fb95a3363aebf15
nevisadhiambo <nevisadhiambo71@gmail.com>
2023-02-21 05:18:59 -0800

    made changes to task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *print_alphabet_x10 - prints 10 times the alphabet, in lowercase
- * Return: Always 0.
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
  */
 void print_alphabet_x10(void)
 {
